e1253b40d4d89077712086e44b87adae7d1f9e6f
rpivi <rickpiwi25@gmail.com>
2024-04-13 11:02:49 +0200

    aggiunto clang-format e modifiche a boids

--- boids.cpp
+++ boids.cpp
@@ -7,6 +7,13 @@ struct two_d {
   double y;
 };
 
+two_d operator+(two_d const& a, two_d const& b) {
+  return two_d{a.x + b.x, a.y + b.y};
+}
+two_d operator*(two_d const& a, auto const& b) {
+  return two_d{a.x * b, a.y * b};
+}
+
 class Boid {
  private:
   two_d position_;
@@ -27,35 +34,19 @@ class Boid {
   }
   two_d update_v(two_d const& v1, two_d const& v2, two_d const& v3) {
     velocity_ = velocity_ + v1 + v2 + v3;
-    // overload dell'operatore + da fare per 2d
   }
+
   two_d update_p(double delta_t) {
     assert(delta_t > 0);
     // delta_t deve essere positivo
     position_ = position_ + velocity_ * delta_t;
-    // overload dell'operatore * da fare per 2d (in caso si moltiplichi per una
-    // costante)
   }
 };
 
-class Flock {
- private:
-  std::vector<Boid> formation_;
+int main() {
 
- public:
-  Flock() {}  // costruttore e classe da implementare
-};
+std::vector<Boid> flock;
 
-two_d separation(double s, double radius) {
-  // da implementare, restituisce v1
+
+
 }
-
-two_d allineament(double a) {
-  // da implementare, restituisce v2
-}
-
-two_d cohesion(double c) {
-  // da implementare, restituisce v3
-}
-
-int main() {}
