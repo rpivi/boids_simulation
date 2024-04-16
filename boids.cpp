9eb672fa4dc6650953c0f4617e7658c659c9e4a8
rpivi <rickpiwi25@gmail.com>
2024-04-16 10:15:11 +0200

    matrix allocation of information, first output of the project

--- boids.cpp
+++ boids.cpp
@@ -1,25 +1,26 @@
+#include <array>
 #include <cassert>
 #include <cmath>
+#include <iostream>
 #include <random>
-#include <vector>
 
 // random generation of number
 ///////////////////////////////////////////////////////////////////////////////////////////
 std::default_random_engine eng;
 std::uniform_int_distribution<> roll_dice(1, 100);
 
-// two dimensional rappresewntation
+// two dimensional rappresentation
 ////////////////////////////////////////////////////////////////////////////////////////////
 struct two_d {
-  double x;
-  double y;
+  int x;
+  int y;
 };
 
 // overloading operator
 two_d operator+(two_d const& a, two_d const& b) {
   return two_d{a.x + b.x, a.y + b.y};
 }
-two_d operator*(two_d const& a, auto const& b) {
+two_d operator*(two_d const& a, int const& b) {
   return two_d{a.x * b, a.y * b};
 }
 
@@ -38,30 +39,45 @@ class Boid {
   two_d p() const { return position_; }
   two_d v() const { return velocity_; }
 
-  bool near(Boid const& other, double radius) const {
+  bool near(Boid const& other, double radius) {
     if (std::sqrt(pow(position_.x - other.position_.x, 2)) +
             pow(position_.y - other.position_.y, 2) <=
         radius) {
       return true;
-    }
+    } else
+      return false;
   }
-  two_d update_v(two_d const& v1, two_d const& v2, two_d const& v3) {
+  void update_v(two_d const& v1, two_d const& v2, two_d const& v3) {
     velocity_ = velocity_ + v1 + v2 + v3;
   }
 
-  two_d update_p(double delta_t) {
+  void update_p(double delta_t) {
     assert(delta_t > 0);
     position_ = position_ + velocity_ * delta_t;
   }
 };
 
+// le 3 leggi
+/////////////////////////////////////////////////////////////////////////////////////////////
+
 // Main
 /////////////////////////////////////////////////////////////////////////////////////////////
 int main() {
-  int num_boids;
+  double radius{45};
+  int num_boids{5};
   std::vector<Boid> flock;
+  bool info_near[num_boids][num_boids];
 
   for (int i = 0; i < num_boids; ++i) {
     flock.push_back(Boid());
   }
+
+  for (int row = 0; row < num_boids; ++row)
+    for (int col = 0; col < num_boids; ++col) {
+      if (flock[row].near(flock[col], radius)) {
+        info_near[row][col] = true;
+      } else
+        (info_near[row][col] = false);
+      std::cout << info_near[row][col];
+    }
 }
