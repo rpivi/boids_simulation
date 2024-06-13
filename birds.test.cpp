aa72381cb0d92dfbd486d8a28e81e4455d20ac43
Mattia <mattia.schirripa04@gmail.com>
2024-06-13 13:21:13 +0200

    fix

--- birds.test.cpp
+++ birds.test.cpp
@@ -9,7 +9,7 @@ TEST_CASE("Testing the near function ") {
     double d = 50;
     birds::Boid p1{1., 0.,1.,1.};
     birds::Boid p2{1., 1., 1., 1.};
-    CHECK( birds::Boid::near(p1 , d)const==true);
+    CHECK (p2.near(p1 , d)==true);
   }
 }
 
